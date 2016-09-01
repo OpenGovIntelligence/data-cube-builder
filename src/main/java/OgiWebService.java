460af0167115afbd12ac765d47c5501f2ac435fd
Mohamed Adel <mohamed.adel@insight-centre.org>
2016-09-01 17:45:36 +0100

    commenting

--- src/main/java/OgiWebService.java
+++ src/main/java/OgiWebService.java
@@ -7,37 +7,42 @@ public class OgiWebService {
 	static String marineDatasetName;
 	static String serialization;
 	static String cubePath;
-	String dimOrMeasures;
+	static String dimOrMeasures;
 	static TarqlFormulator tarqlformulator;
-	
+
 	public static void main(String args[]) {
 
-		tarqlformulator =new TarqlFormulator();
-		
-		
+		tarqlformulator = new TarqlFormulator();
+
 		// port(8080);
+		// host:4567/cubeBuilderArgs?csv=inputFileNameAndLocation&schema=marineInstituteDatasetId&serialization=turtle&cube=outputFileAndLocation
+		//http://localhost:4567/cubeBuilderArgs?csv=src%2Fmain%2Fresources%2Fdata%2FIWaveBNetwork30Min.csv&schema=IWaveBNetwork30Min&serialization=turtle&cube=src%2Fmain%2Fresources%2Foutput%2Fwebservice.ttl
 		get("cubeBuilderArgs", "application/json", (request, response) -> {
 			csvPath = request.queryParams("csv");
 			marineDatasetName = request.queryParams("schema");
 			serialization = request.queryParams("serialization");
 			cubePath = request.queryParams("cube");
 
-			return "HI ";
+			return run();
 		});
 
 	}
 
-	public String run() {
+	public static String run() {
 
-		if (serialization.equalsIgnoreCase("turtle"))
-			tarqlformulator.tarqlExcution(csvPath, cubePath, dimOrMeasures,
-					marineDatasetName, " ");
-		else
-			tarqlformulator.tarqlExcution(csvPath, cubePath, dimOrMeasures,
-					marineDatasetName, serialization);
-
-		return null;
+		try {
+			if (serialization.equalsIgnoreCase("turtle")) {
+				tarqlformulator.tarqlExcution(csvPath, cubePath, dimOrMeasures,
+						marineDatasetName, " ");
+				return "Success: Cube Created check distination folder!";
+			} else {
+				tarqlformulator.tarqlExcution(csvPath, cubePath, dimOrMeasures,
+						marineDatasetName, serialization);
+				return "Success: Cube Created check distination folder!";
+			}
+		} catch (Exception ex) {
+			return "Error: "+ex.getMessage();
+		}
 
 	}
-
 }
